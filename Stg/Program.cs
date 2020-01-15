e7f7c9efd0a65e8e990f946eded2b8d6128c73bd
a.barkin <a.m.barkin@gmail.com>
2020-01-15 18:26:17 +0300

    first

---
+++ Stg/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stg
+{
+    internal class Program
+    {
+        internal const int Density = 1;
+        internal const int Version = 101;
+
+        private static void Main(string[] args)
+        {
+            var packer = new Packer();
+            packer.Pack(@"C:\Temp\nature.png", @"c:\Temp\AmCre\AmCre-1.mp4", @"c:\Temp\out.png", Density);
+
+            //var unpacker = new Unpacker();
+            //unpacker.Unpack(@"c:\Temp\out.png", @"c:\Temp\out", Density);
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
